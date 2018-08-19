54d09f8ebf10a8f798bd9d59b71eb328ed550c13
clc80 <clc_80@yahoo.com>
2018-08-18 21:08:29 -0700

    modified contactController.js

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -4,7 +4,6 @@ const sequelize = require("../db/models/index").sequelize;
 describe("ContactController", () => {
   beforeEach((done) => {
     this.book = new ContactController();
-
     sequelize.sync({force: true}).then((res) => {
       done();
     })
@@ -32,3 +31,31 @@ describe("#addContact()", () => {
   });
 });
 })
+
+describe("#getContacts()", () => {
+  it("should return an empty array when no contacts are available", (done) => {
+    this.book.getContacts()
+    .then((contacts) => {
+      expect(contact.length).toBe(0);
+      done();
+    })
+    .catch((err) => {
+      console.log(err);
+      done();
+    });
+  });
+  it("should return an array of contacts when contacts are available", (done) => {
+    this.book.addContact("Alice", "001-101-1010", "alice@example.com")
+    .then(() => {
+      this.book.getContacts()
+      .then((contacts) => {
+        expect(contacts.length).toBe(1);
+        done();
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      done();
+    });
+  });
+});
